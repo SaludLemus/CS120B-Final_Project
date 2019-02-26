11b94ca2416bf032d472a6451f8e326b79d0b33f
SaludLemus <saludlemus1@gmail.com>
2019-02-26 10:37:32 -0800

    Renamed .c file

---
+++ main_driver.c
@@ -0,0 +1,21 @@
+/*
+ * main_driver.c
+ *
+ * This .c file contains the main driver components for the
+ * project
+ *
+ * Created: 2/26/2019 10:15:10 AM
+ * Author : Salud Lemus
+ */ 
+
+#include <avr/io.h>
+
+
+int main() {
+	
+	
+	while (1) {
+		
+	}
+}
+
